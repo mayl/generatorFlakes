6290913f55810d6c02d359c9a5408789058dd8af
Larry May <larrymayster@gmail.com>
2021-11-04 14:40:06 -0400

    initial commit, virtualbox works

---
+++ configuration.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+{
+  services.sshd.enable = true;
+  services.nginx.enable = true;
+
+  networking.firewall.allowedTCPPorts = [ 80 ];
+  
+  users.users.root.password = "nixos";
+  services.openssh.permitRootLogin = lib.mkDefault "yes";
+  services.getty.autologinUser = lib.mkDefault "root";
+
+  environment.systemPackages = [
+    pkgs.cowsay
+  ];
+}
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Build VMs with flakes";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, nixos-generators, ...}: 
+  let
+    system  = "x86_64-linux";
+    pkgs = import nixpkgs{
+      inherit system;
+    };
+  in {
+    vbox = (nixpkgs.lib.nixosSystem {
+      inherit system pkgs;
+      modules = [ 
+        nixos-generators.nixosModules.virtualbox
+        ./configuration.nix
+      ];
+    }).config.system.build.virtualBoxOVA;
+  };
+}
