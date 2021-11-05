38cc8f710d2628215d92c3fba6685d2b13ee063f
Larry May <larrymayster@gmail.com>
2021-11-04 21:33:50 -0400

    use my nixos-generators fork
    re-point this at my fork for testing the `nixosGenerate` function

--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixos-generators = {
-      url = "github:nix-community/nixos-generators";
+      # url = "github:nix-community/nixos-generators";
+      url = "github:mayl/nixos-generators";
+      # url = "path:/home/larry/projects/nix/nixos-generators";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -15,22 +17,37 @@
     pkgs = import nixpkgs{
       inherit system;
     };
+    nixosGenerate = nixos-generators.nixosGenerate;
 
-    nixosGenerate = { system, pkgs, modules, format, formatAttr }:
-    let 
-      formatModule = builtins.getAttr format nixos-generators.nixosModules;
-    in
-    builtins.getAttr formatAttr (nixpkgs.lib.nixosSystem { 
-      inherit system pkgs;
-      modules = [
-        formatModule
-      ] ++ modules;
-    }).config.system.build;
   in {
-    vbox = nixosGenerate{ inherit system pkgs; modules = [./configuration.nix]; format = "virtualbox"; formatAttr = "virtualBoxOVA";};
-    vagrant = nixosGenerate{ inherit system pkgs; modules = [./configuration.nix]; format = "vagrant-virtualbox"; formatAttr = "vagrantVirtualbox"; };
-    vmware = nixosGenerate { inherit system pkgs; modules = [./configuration.nix]; format = "vmware"; formatAttr = "vmwareImage"; };
-    lxc = nixosGenerate { inherit system pkgs; modules = [./configuration.nix]; format = "lxc"; formatAttr = "tarball"; };
-    sd = nixosGenerate { pkgs=pkgs.pkgsCross.aarch64-multiplatform; system="aarch64-linux";  modules = [./configuration.nix "${pkgs.path}/nixos/modules/profiles/minimal.nix" ]; format = "sd-aarch64"; formatAttr = "sdImage"; };
+    vbox = nixosGenerate{ 
+      inherit system pkgs;
+      modules = [./configuration.nix];
+      format = "virtualbox"; 
+    };
+
+    vagrant = nixosGenerate{
+      inherit system pkgs;
+      modules = [./configuration.nix];
+      format = "vagrant-virtualbox"; 
+    };
+
+    vmware = nixosGenerate { 
+      inherit system pkgs;
+      modules = [./configuration.nix];
+      format = "vmware";
+    };
+
+    lxc = nixosGenerate {
+      inherit system pkgs;
+      modules = [./configuration.nix];
+      format = "lxc";
+    };
+
+    sd = nixosGenerate { 
+      pkgs=pkgs.pkgsCross.aarch64-multiplatform;
+      system="aarch64-linux";
+      modules = [./configuration.nix "${pkgs.path}/nixos/modules/profiles/minimal.nix" ];
+      format = "sd-aarch64"; };
   };
 }
